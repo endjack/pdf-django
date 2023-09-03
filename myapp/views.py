2438419e0eb2533d8d720e5d66a58d2a49e87a8a
Enderson Jackson <endersonjackson2@gmail.com>
2023-09-03 15:12:15 -0300

    Update View Project

--- myapp/views.py
+++ myapp/views.py
@@ -1,20 +1,15 @@
-import os
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import View
-
 from pdfgenerator import settings
-from .utils import render_to_pdf
 from django.template.loader import render_to_string
-
+import pdfkit
+import os
 
 def index(request):
     return render(request, 'index.html')
 
     
-
 def pdf_from_template(request, type):
-    import pdfkit
 
     #Template com Dados
     context = {
@@ -51,20 +46,4 @@ def pdf_from_template(request, type):
     return HttpResponse("Page Not Found")
 
 
-
-class GeneratePdf(View):
-    def get(self, request, *args, **kwargs):
-        data = {
-        "name": "Mama", #you can feach the data from database
-        "id": 18,
-        "amount": 333,
-        }
-        pdf = render_to_pdf('report.html',data)
-        if pdf:
-            response=HttpResponse(pdf,content_type='application/pdf')
-            filename = "Report_for_%s.pdf" %(data['id'])
-            content = "inline; filename= %s" %(filename)
-            response['Content-Disposition']=content
-            return response
-        return HttpResponse("Page Not Found")
 
